350c3bec1a064c6b2f0144822ce61f51f85fd556
leo-a-siqueira <leoapss151@gmail.com>
2024-02-21 20:23:52 -0300

    inclusão das listas

---
+++ Listas/Lista1/Exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("Seja muito bem-vindo ao sistema de cálculo de média das notas de aluno.\n Abaixo digite a 1° nota:");
+        Double nota1 = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a 2° nota:");        
+        Double nota2 = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a 3° nota:");
+        Double nota3 = Double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a 4° nota:");
+        Double nota4 = Double.Parse(Console.ReadLine());
+
+        //Processamento
+        Double resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        Console.WriteLine("A média do aluno é " + resultado);
+
+    }
+}
