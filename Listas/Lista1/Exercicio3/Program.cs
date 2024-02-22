e1bbe99c742842d2abc535a84e23866796b4abd9
leo-a-siqueira <leoapss151@gmail.com>
2024-02-21 21:14:12 -0300

    Exercício3.

---
+++ Listas/Lista1/Exercicio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{   
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("Bem vindo ao contador de moedas de 1 centavo!");
+        Console.WriteLine("Digite a quantidade de moedas de 1 centavo:");
+        int moedas = int.Parse(Console.ReadLine());
+        
+        int reais = moedas / 100;
+        int centavos = moedas % 100;
+
+        Console.WriteLine("O total é " + reais + " Real(is) e " + centavos + " Centavo(s).");
+        
+    }
+}
