e217a173ea7435b8aabf2362c1e99e9f1ab94f62
leo-a-siqueira <leoapss151@gmail.com>
2024-02-29 21:20:52 -0300

    envio do exercio 1

---
+++ Listas/Lista2/Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        const string name = "Juca";
+        const string password = "123senha";
+
+        Console.WriteLine("Informe o usuario abaixo:");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("Informe a senha abaixo:");
+        string  userPassword = Console.ReadLine();
+
+        if (name == userName && password == userPassword)
+        {
+            Console.WriteLine("Login realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Credenciais invalidas!");
+        }
+    }
+}
