7a47b2ffd678ddb4251836ec08b13caa034bc317
leo-a-siqueira <leoapss151@gmail.com>
2024-02-29 21:34:31 -0300

    Envio do exercicio 2.

---
+++ Listas/Lista2/Exercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a nota 1:");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 2:");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 3:");
+        double nota3 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a nota 4:");
+        double nota4 = double.Parse(Console.ReadLine());
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        Console.WriteLine("A media eh: " + media);
+
+        if (media < 5 && media > 3)
+        {
+            Console.WriteLine("Aluno em recuperacao.");
+        }
+        else if (media < 3)
+        {
+            Console.WriteLine("Aluno reprovado.");
+        }
+        else
+        {
+            Console.WriteLine("Aluno aprovado.");
+        }
+    }
+}
