87054dbffb223e82caebedec371fc1263fa39435
leo-a-siqueira <leoapss151@gmail.com>
2024-03-02 12:36:09 -0300

    ipomrtação de piano.

---
+++ Aulas/Aula6/projeto-piano/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        int contador = 1;
+        const int finalNota = 7;
+
+
+        while (contador <= finalNota)
+        {
+            Piano.ReproduzTecla(3, contador);
+            contador++;
+        }
+        Piano.ReproduzTecla(4,1);
+
+      
+
+        
+    }
+}
