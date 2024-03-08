5622b9fea911359ef6db777befc9680f12d8f9f5
leo-a-siqueira <leoapss151@gmail.com>
2024-03-08 20:53:50 -0300

    Exercicio 2.

---
+++ Listas/Lista3/Atividade2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const double freteGratis = 150;
+        double valorCompra = 0;
+        int i = 1;
+
+        Console.WriteLine("Digite a quantidade de intens a comprar:");
+        int quantidadeItens = int.Parse(Console.ReadLine());
+
+        while (quantidadeItens >= i )
+        {
+            Console.WriteLine("Digite o valor do " + i + "° produto:" );
+            valorCompra += double.Parse(Console.ReadLine());
+            i++;
+        }
+        
+        if( valorCompra > freteGratis )
+        {
+            Console.WriteLine("\nParabens! Voce ganhou frete gratis em sua compra de valor R$ " + Math.Round(valorCompra , 2) + ".");
+        }
+        else
+        {
+            Console.WriteLine("\nO valor da sua compra eh R$ " + Math.Round(valorCompra) + ".");
+        }
+        
+    }
+}
