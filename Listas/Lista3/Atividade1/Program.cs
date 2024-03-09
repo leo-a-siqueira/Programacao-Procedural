3a9c0eb946fed5869dba5bcceb91fcc1de47d7e8
leo-a-siqueira <leoapss151@gmail.com>
2024-03-08 21:04:11 -0300

    Upload da lista 3.

---
+++ Listas/Lista3/Atividade1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         const int pessoas = 20;
+         double salarioDigitado = 0;
+         double salarioTotal = 0;
+         double maiorSalario = 0;
+         double filhos = 0;
+
+        
+         for(int i = 1 ; i <= pessoas ; i++)
+         {
+            
+            Console.WriteLine("Digite o salario da " + i + "° pessoa:");
+            salarioDigitado = Convert.ToDouble(Console.ReadLine());
+            salarioTotal += salarioDigitado;
+
+            Console.WriteLine("Digite a quantidade de filhos da " + i + "° pessoa:");
+            filhos += Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            if( salarioDigitado > maiorSalario )
+            {
+                maiorSalario = salarioDigitado;
+            }
+         }
+         System.Console.WriteLine( filhos + " " + pessoas);
+         double mediaFilhos = (filhos / pessoas);
+         double mediaSalarios = (salarioTotal / pessoas);
+
+         Console.WriteLine("Media salarial =  R$ " +  Math.Round(mediaSalarios, 2) + ".");
+         Console.WriteLine("Media do numero de filhos por habitante = " + Math.Round(mediaFilhos, 2) + ".");
+         Console.WriteLine("Maior salario = R$ " + maiorSalario + ".");
+    }
+}
---
+++ Listas/Lista3/Atividade3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Prgram
+{
+    static void Main(string [] args)
+    {
+        const string usuario = "admin";
+        const string senha = "123senha";
+        const int numberoDeTentativas = 3;
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine("Digite o nome de usuario:");
+            string usuarioDigitado = Console.ReadLine();
+            
+            Console.WriteLine("Digite a senha:");
+            string senhaDigitada = Console.ReadLine();
+
+            if( usuario == usuarioDigitado && senha == senhaDigitada )
+            {
+                Console.WriteLine("Login realizado com sucesso!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("\nCredenciais inválidas!");
+                Console.WriteLine("Voce tem " + (numberoDeTentativas - i) + " tentativa(s).\n\n");
+                i++;
+            }            
+
+        }
+        while( i <= numberoDeTentativas);
+        
+        if (i > numberoDeTentativas)
+        {
+            Console.WriteLine("Sua conta foi bloqueada!");
+        }
+    }
+}
