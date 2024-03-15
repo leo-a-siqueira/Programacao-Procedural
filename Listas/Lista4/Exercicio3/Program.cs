d718aeed225a57703857264957d1402538ce8e10
leo-a-siqueira <leoapss151@gmail.com>
2024-03-15 13:12:30 -0300

    upload exercicio 3

---
+++ Listas/Lista4/Exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();         
+
+        for(int i = 0; i < 8; i ++)
+        {
+            Tabuleiro.Adicionar(0, i, "s");
+            Tabuleiro.Adicionar(i, 0, "s");
+            Tabuleiro.Adicionar(i, 7, "s");
+            Tabuleiro.Adicionar(7, i, "s");
+        }
+
+        Tabuleiro.ImprimeTabuleiro();
+         
+    }
+}
