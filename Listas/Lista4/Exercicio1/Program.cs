8c66f0745b250f58fb9c6caf42d4c6e33d1b9f6c
leo-a-siqueira <leoapss151@gmail.com>
2024-03-15 12:13:31 -0300

    upload exercicio 1

---
+++ Listas/Lista4/Exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int numero1 = 1; numero1 <= 10; numero1++)
+        {
+            for(int numero2 = 1; numero2 <= 10; numero2++)
+            {
+                Console.WriteLine(numero1 + " x " + numero2 + " = " + (numero1 * numero2));
+            }
+            Console.WriteLine("");
+        }
+    }
+}
