5dca803ac3a7680a83649061f511bb8e394df63f
leo-a-siqueira <leoapss151@gmail.com>
2024-03-19 22:39:05 -0300

    upload exercicio 5

---
+++ Listas/Lista5/Exercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();
+
+        for (int linha = 0; linha <= 7; linha++)
+        {
+            if (linha % 2 == 0)
+            {
+                for (int coluna = 0; coluna <= 7; coluna++)
+                {
+                    Tabuleiro.Adicionar(linha, coluna, "C");
+                }
+            }
+        }
+
+        Tabuleiro.ImprimeTabuleiro();
+    }
+}
