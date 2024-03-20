51b59465f8867cae60a3693d1c2a0f75de9d441f
leo-a-siqueira <leoapss151@gmail.com>
2024-03-19 22:08:28 -0300

    upload exercicio2

---
+++ Listas/Lista5/Exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i = 10; i > 0 ; i--)
+        {
+            for(int i2 = 0 ; i2 <= 10 ; i2++)
+            {
+                Console.WriteLine(i + " x " + i2 + " = " + (i * i2));
+            }
+            Console.WriteLine();
+        }
+    }
+}
