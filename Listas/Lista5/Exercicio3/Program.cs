35850a340c037259621cdc87ef78b9e10e21aadf
leo-a-siqueira <leoapss151@gmail.com>
2024-03-19 22:17:59 -0300

    upload exercicio 3

---
+++ Listas/Lista5/Exercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a altura:");
+        int linha = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite a base:");
+        int coluna = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        for(int i = 0 ; i < linha ; i ++)
+        {
+            for(int i2 = 0 ; i2 < coluna ; i2++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
