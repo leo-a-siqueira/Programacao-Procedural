ad3d47e6028c3a36f5eed7c356cacfb222df887d
leo-a-siqueira <leoapss151@gmail.com>
2024-03-19 21:56:37 -0300

    upload exercicio 1

---
+++ Listas/Lista5/Exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero1 = 0;
+        int numero2 = 200;
+
+        for(int i = 0 ; i <= 50 ; i++)
+        {
+            Console.WriteLine(numero1 + " - " + numero2);
+            numero1 += 2; 
+            numero2 -= 4;            
+        }
+    }
+}
