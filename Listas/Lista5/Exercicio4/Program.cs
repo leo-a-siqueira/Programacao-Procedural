293ac565773e952e606ef93f271082a6c563e7be
leo-a-siqueira <leoapss151@gmail.com>
2024-03-19 22:30:38 -0300

    upload exercicio 4

---
+++ Listas/Lista5/Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Prgram
+{
+    static void Main(string[] args)
+    {
+        int numeroDigitado = 0;
+        int somatorio = 0;
+
+        do
+        {
+            Console.WriteLine("Digite um numero inteiro positivo:");
+            numeroDigitado = int.Parse(Console.ReadLine());
+
+            if (numeroDigitado > 0 && numeroDigitado % 2 != 0)
+            {
+                somatorio = somatorio + numeroDigitado;
+            }
+
+        }
+        while (numeroDigitado >= 0);
+
+        Console.WriteLine("\nO somatorio de impares eh: " + somatorio);
+    }
+}
