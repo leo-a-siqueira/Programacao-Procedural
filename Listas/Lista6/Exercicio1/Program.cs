51c07242605786eab8122912e54207f927e3cd8b
leo-a-siqueira <leoapss151@gmail.com>
2024-03-27 21:47:48 -0300

    upload exercicio 1

---
+++ Listas/Lista6/Exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int quatidadeDePessoas = 10;
+        string[] nomes = new string[quatidadeDePessoas];
+        bool resultadoPesquisa = false;
+
+        for (int i = 0; i < quatidadeDePessoas; i++)
+        {
+            nomes[i] = Console.ReadLine();
+        }
+
+        string pesquisaNome = Console.ReadLine();
+
+        for (int i = 0; i < quatidadeDePessoas; i++)
+        {
+            if (pesquisaNome == nomes[i])
+            {
+                resultadoPesquisa = true;
+            }
+        }
+
+        if (resultadoPesquisa == true)
+        {
+            Console.WriteLine("ACHEI");
+        }
+        else
+        {
+            Console.WriteLine("NAO ACHEI");
+        }
+
+    }
+}
