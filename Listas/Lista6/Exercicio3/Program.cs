e7c527834d1ae9c595eaca2642f8d40c284d2011
leo-a-siqueira <leoapss151@gmail.com>
2024-03-27 22:11:42 -0300

    upload exercicio 3

---
+++ Listas/Lista6/Exercicio3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        const int matrizTamanho = 3;
+        int[,] matriz = new int[matrizTamanho, matrizTamanho];
+        int soma = 0;
+
+        for (int i = 0; i < matrizTamanho; i++)
+        {
+            for (int j = 0; j < matrizTamanho; j++)
+            {
+
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < matrizTamanho; i++)
+        {
+            for (int j = 0; j < matrizTamanho; j++)
+            {
+                if(i == j)
+                {
+                    soma += matriz[i, j];
+                }
+                
+            }
+        }
+
+        Console.WriteLine(soma);
+    }
+}
