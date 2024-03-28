8f234c47acea6a00d7bdcf77f5506763b8deaecf
leo-a-siqueira <leoapss151@gmail.com>
2024-03-27 21:56:19 -0300

    upload exercicio 2

---
+++ Listas/Lista6/Exercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int quantidadeNomes = 10;
+        string[] nomes = new string[quantidadeNomes];
+
+        for (int i = 0; i < quantidadeNomes; i++)
+        {
+            nomes[i] = Console.ReadLine();
+        }
+
+        for (int i = quantidadeNomes - 1; i >= 0; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+        }
+    }
+}
