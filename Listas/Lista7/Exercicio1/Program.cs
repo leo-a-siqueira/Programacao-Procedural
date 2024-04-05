27594bef951aa45edc960d4b551aef7b72a0d6bf
leo-a-siqueira <leoapss151@gmail.com>
2024-04-04 21:12:37 -0300

    upload exercicio 1

---
+++ Listas/Lista7/Exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        verificaPar(Convert.ToInt32(Console.ReadLine()));
+    }
+    static void verificaPar(int numero)
+    {
+        Console.WriteLine(numero % 2 == 0);
+    }
+}
