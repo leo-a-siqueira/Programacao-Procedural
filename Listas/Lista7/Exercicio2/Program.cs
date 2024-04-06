9fccb49e91768e35bb6d940d0829f0e7d5d73d7e
leo-a-siqueira <leoapss151@gmail.com>
2024-04-05 21:52:29 -0300

    Upload Exercicios

---
+++ Listas/Lista7/Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = lerNumeros();
+        imprimirMaiorNumero(num);
+    }
+    static int[] lerNumeros()
+    {
+        const int tamanhoVetor = 3;
+        int[] numeros = new int[tamanhoVetor];
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return numeros;
+    }
+
+    static void imprimirMaiorNumero(int[] numeros)
+    {
+        int numeroMaior = 0;
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (numeroMaior < numeros[i])
+            {
+                numeroMaior = numeros[i];
+            }
+        }
+        Console.WriteLine(numeroMaior); ;
+    }
+}
---
+++ Listas/Lista7/Exercicio5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        const int tamanhoMatriz = 4;
+        int[,] matriz = new int[tamanhoMatriz, tamanhoMatriz];
+
+        matriz = PreencherMatriz(tamanhoMatriz);
+        bool validador = VerificarBalistas(matriz);
+
+        if (validador == true)
+        {
+            Console.WriteLine("Fique atento! existem balistas no tabuleiro");
+        }
+        else
+        {
+            Console.WriteLine("nao ha balistas no tabuleiro");
+        }
+    }
+
+    static int[,] PreencherMatriz(int tamanhoMatriz)
+    {
+        int[,] matriz = new int[tamanhoMatriz, tamanhoMatriz];
+
+        for (int i = 0; i < tamanhoMatriz; i++)
+        {
+            for (int j = 0; j < tamanhoMatriz; j++)
+            {
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        return matriz;
+    }
+
+    static bool VerificarBalistas(int[,] matriz)
+
+    {
+        const int tamanhoMatriz = 4;
+
+        bool validador = false;
+
+        for (int i = 0; i < tamanhoMatriz; i++)
+        {
+            for (int j = 0; j < tamanhoMatriz; j++)
+            {
+                if (matriz[i, j] == 0)
+                {
+                    validador = true;
+                }
+            }
+        }
+        return validador;
+    }
+}
