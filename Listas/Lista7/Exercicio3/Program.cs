7e36ee24c118f4d654b63b8dd004cea9a38ce78f
leo-a-siqueira <leoapss151@gmail.com>
2024-04-05 21:53:38 -0300

    upload exercicios

---
+++ Listas/Lista7/Exercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassificarNadador(Convert.ToInt32(Console.ReadLine()));
+    }
+    static void ClassificarNadador(int idade)
+    {
+        if (idade < 12)
+        {
+            Console.WriteLine("Infantil");
+        }
+        else if (idade >= 12 && idade <= 14)
+        {
+            Console.WriteLine("Juvenil A");
+        }
+        else if (idade >= 15 && idade <= 17)
+        {
+            Console.WriteLine("Juvenil B");
+        }
+        else
+        {
+            Console.WriteLine("Adulto");
+        }
+    }
+}
---
+++ Listas/Lista7/Exercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int tamanhoTrem = 12;
+        int[] vagoes = new int[tamanhoTrem];
+
+        vagoes = PreencherTrem(tamanhoTrem);
+
+        VerificarPeso(vagoes);
+    }
+    static int[] PreencherTrem(int tamanhoTrem)
+    {
+        int[] vagoes = new int[tamanhoTrem];
+        for (int i = 0; i < vagoes.Length; i++)
+        {
+            vagoes[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        return vagoes;
+    }
+
+    static void VerificarPeso(int[] vagoes)
+    {
+        const int pesoMaximo = 50;
+        for (int i = 0; i < vagoes.Length; i++)
+        {
+            if (vagoes[i] > pesoMaximo)
+            {
+                Console.WriteLine((i + 1) + "° vagao ultrapassou o limite de peso");
+                
+            }
+        }
+    }
+}
