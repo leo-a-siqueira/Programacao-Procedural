91e15e4d0925caf5e96bfae2d2febb877de5db82
leo-a-siqueira <leoapss151@gmail.com>
2024-04-12 20:20:05 -0300

    upload exercicio 1

---
+++ Listas/Lista8/Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+struct pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        pizza pizza1;
+        pizza1.id = Convert.ToInt32(Console.ReadLine());
+        pizza1.sabor = Console.ReadLine();
+        pizza1.preco = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(pizza1.id + "\n" + pizza1.sabor + "\n" + pizza1.preco);
+    }
+}
