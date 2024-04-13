6ce5b5f52e6e2e210685e4f2ce13fcced96f4b30
leo-a-siqueira <leoapss151@gmail.com>
2024-04-12 21:33:57 -0300

    upload exercicio 2

---
+++ Listas/Lista8/Exercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    struct Produto
+    {
+        public int id;
+        public string nome;
+        public double preco;
+        public bool disponivelEmEstoque;
+    }
+
+    static void Main(string[] args)
+    {
+        const int tamanhoVetor = 4;
+        Produto[] produtos = new Produto[tamanhoVetor];
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            produtos[i].id = Convert.ToInt32(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            if (produtos[i].disponivelEmEstoque == true)
+            {
+                Console.WriteLine(produtos[i].nome);
+                Console.WriteLine(produtos[i].preco.ToString("F"));
+            }
+        }
+
+
+    }
+}
